e216c7086fc2851d1a81770472edf6e49f6bfadf
sinemnagod <sinem121995@hotmail.com>
2025-05-30 00:25:39 +0300

    back to old self

--- src/main/java/com/recipe_app/backend/config/WebConfig.java
+++ src/main/java/com/recipe_app/backend/config/WebConfig.java
@@ -9,14 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     // added
-    @Value("${frontend_url}")
-    private String frontendUrl;
+    /*
+     * @Value("${frontend_url}")
+     * private String frontendUrl;
+     */
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                // .allowedOrigins("http://localhost:3001")
-                .allowedOrigins(frontendUrl)
+                .allowedOrigins("http://localhost:3001")
+                // .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
