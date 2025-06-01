0460303635b9cae7918e94e523dad06cae91b8f7
sinemnagod <sinem121995@hotmail.com>
2025-06-01 23:25:46 +0300

    fix: Update allowed origins to add appropiate host

--- src/main/java/com/recipe_app/backend/config/SecurityConfig.java
+++ src/main/java/com/recipe_app/backend/config/SecurityConfig.java
@@ -35,7 +35,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("https://my-recipe-frontend.vercel.app"));
+        configuration.setAllowedOrigins(Arrays.asList(
+        "https://my-recipe-frontend.vercel.app",
+        "https://my-recipe-frontend-jozwkz115-sinems-projects-da5e3fd0.vercel.app",
+        "http://localhost:3001"
+    ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
