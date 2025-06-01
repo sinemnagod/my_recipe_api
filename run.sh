3eb275b6d159f8d5a6310f037103afcbb96f2a27
sinemnagod <sinem121995@hotmail.com>
2025-06-01 20:06:15 +0300

    fix: update database configuration and add run script

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set environment variables
+export SPRING_DATASOURCE_USERNAME=root
+export SPRING_DATASOURCE_PASSWORD=iygrUyKhbTGRblmQhJVHYPeTKrfYfire
+export PORT=8081
+
+# Run the application
+./mvnw spring-boot:run 
